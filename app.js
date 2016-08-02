7593a9320f6886b1c63258ebea529c30975f2a85
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 12:01:23 +0530

    Changes for APP setting command

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
+	res.write('<p>Hello from ' + process.env.CloudProvider +' </p>');
+res.write('<ul>');
+res.write('<li><label>Service Bus Namespace:</label>'+ process.env.ServiceBusConnectionString +' </li>');
+res.write('<li><label>Storage Connection String:</label>'+ process.env.StorageConnectionString +' </li></ul></p>')
  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
