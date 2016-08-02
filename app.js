0fec825c1762889ae2682589971505cb139e3ca7
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 19:27:48 +0530

    Node app Deployed

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+    //res.sendFile(__dirname + '/index.html');
+    res.write('<p>Hello from ' + process.env.mediavaletbaseurl + ' </p>');
+});
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+http.listen(port, function () {
+    console.log('listening on ' + port);
+});
+
+
+/*
+
 var http = require('http');
 var port = process.env.port || 1337;
 
@@ -14,3 +35,8 @@ http.createServer(function (req, res) {
     res.end('<p>' + process.env.mediavaletbaseurl + '</p>');
 
 }).listen(port);
+
+
+
+
+*/
