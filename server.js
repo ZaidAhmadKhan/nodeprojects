1546edb73d35c878ef4d94d725a3f8f5d7af010c
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 18:25:00 +0530

    API hosting with 443

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/GetBaseUrl', function (req, res) {
     }
 });
 
-var server = app.listen(4043, function () {
+var server = app.listen(443, function () {
 
   var host = server.address().address
   var port = server.address().port
