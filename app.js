79ee64c27a7c7cbff04d61b2ed7503f53e19d23e
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 19:24:13 +0530

    Node Function with Simple Html Page

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 4043;
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
-});
-http.listen(port, function () {
-    console.log('listening on ' + port);
-});
+var http = require('http');
+var port = process.env.port || 1337;
+
+http.createServer(function (req, res) {
+
+    res.write('<p>Hello from ' + process.env.mediavaletbaseurl + ' </p>');
+    res.write('<ul>');
+    res.write('<li><label>Service Bus Namespace:</label>' + process.env.mediavaletbaseurl + ' </li>');
+    res.write('<li><label>Storage Connection String:</label>' + process.env.mediavaletbaseurl + ' </li>');
+    res.write('</ul>');
+
+    res.write('<p>Happy Clouding!</p>');
+
+    res.end('<p>' + process.env.mediavaletbaseurl + '</p>');
+
+}).listen(port);
