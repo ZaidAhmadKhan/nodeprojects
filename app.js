a23685d5f4fc305ad86cbf229679734ba45c6e03
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 12:12:40 +0530

    Azure App Setting Function Changes

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
-	res.write('<p>Hello from ' + process.env.CloudProvider +' </p>');
-res.write('<ul>');
-res.write('<li><label>Service Bus Namespace:</label>'+ process.env.ServiceBusConnectionString +' </li>');
-res.write('<li><label>Storage Connection String:</label>'+ process.env.StorageConnectionString +' </li></ul></p>')
  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
- io.emit('chat message', msg);
+ io.emit('chat message', process.env.mediavaletbaseurl);
  });
 });
 http.listen(port, function(){
