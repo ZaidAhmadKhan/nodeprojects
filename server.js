771602b4591930534e7a509a75d61667257d8126
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 16:34:38 +0530

    SDK and Node JS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/GetBaseUrl', function (req, res) {
    */
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(4043, function () {
 
   var host = server.address().address
   var port = server.address().port
