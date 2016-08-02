b2fd1060b76a599d71b72bcd5ab9cd96fedf1d1e
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-02 19:17:28 +0530

    Normal APP

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
-/*
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+var port = process.env.PORT || 4043;
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', );
- });
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
-http.listen(port, function(){
- console.log('listening on ' + port);
+http.listen(port, function () {
+    console.log('listening on ' + port);
 });
-*/
-
-
-//process.env.mediavaletbaseurl
-
-var http = require('http');
-var port = process.env.port || 4043;
-http.createServer(function(req, res) {
-
-return process.env.mediavaletbaseurl;
-/*
-res.write('<p>Hello from ' + process.env.mediavaletbaseurl +' </p>');
-res.write('<ul>');
-res.write('<li><label>Azure App Setting Name </label>'+ process.env.mediavaletbaseurl +' </li>');
-
-res.write('</ul>');
-
-res.write('<p>Happy Clouding!</p>');
-
-res.end('<p>'+ process.env.mediavaletbaseurl +'</p>');
-
-*/
-
-}).listen(port);
