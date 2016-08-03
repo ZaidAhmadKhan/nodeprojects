a47c45bb0c81835de23a3762256c58658dd13960
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 21:42:16 +0530

    9:42 Pm

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var http = require('http');
 var port = process.env.port || 4043;
 http.createServer(function(req, res) {
     
-    res.write('<p>Hello from ' + process.env.mediavaletbaseurl +' </p>');
+    res.write('<p>Hello from ' + process.env.mediavaletapp + ' </p>');
     res.write('<ul>');
-    res.write('<li><label>Azure App Setting Name </label>'+ process.env.mediavaletbaseurl +' </li>');
+    res.write('<li><label>Azure App Setting Name </label>' + process.env.mediavaletapp + ' </li>');
     
     res.write('</ul>');
     
     res.write('<p>Happy Clouding!</p>');
     
-    res.end('<p>'+ process.env.mediavaletbaseurl +'</p>');
+    res.end('<p>' + process.env.mediavaletapp + '</p>');
     
     }).listen(port); 
