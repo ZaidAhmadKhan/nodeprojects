55507bdb0d584f8dcd171939b0e7f3e9ff1e1e48
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 20:58:46 +0530

    8:58

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-    if (process.env.ServiceBusConnectionString != undefined) {
-        res.write(process.env.ServiceBusConnectionString);
-        res.wrtie(process.env.ServiceBusConnectionString);
+    if (process.env.mediavaletapp != undefined) {
+        res.write(process.env.mediavaletapp);
+        res.wrtie(process.env.mediavaletapp);
     } else {
         res.write("Null ");
         res.wrtie("Null");
