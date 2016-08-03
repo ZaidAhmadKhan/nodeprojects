11be068c3311b26014da2c1b658a76fcac0b9216
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 22:50:04 +0530

    10.49

--- app.js
+++ app.js
@@ -3,31 +3,17 @@
 var http = require('http');
 var port = process.env.port || 4043;
 http.createServer(function(req, res) {
-    try{
-        if(process.env.mediavaletapp!=undefined){
-            res.write(process.env.mediavaletapp);
-        }else{
-            res.write('value not found');
-        }
-    } catch (e) {
-        res.write('Some Error  ' + e );
-    }
-   
+    
+    res.write('<p>Hello from ' + process.env.mediavaletapp + ' </p>');
+    res.write('<ul>');
+    res.write('<li><label>Azure App Setting Name </label>' + process.env.mediavaletapp + ' </li>');
+    
+    res.write('</ul>');
+    
+    res.write('<p>Happy Clouding!</p>');
+    
+    res.end('<p>' + process.env.mediavaletapp + '</p>');
     
     }).listen(port); 
 
     
-
-    
-
-/*
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-    res.send("ok");
-    res.send(process.env.mediavaletapp);
-
-});
-app.listen(3001);
-console.log('lisening on 3001');
-*/
