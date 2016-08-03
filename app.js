0429eb1cb87ea45e18a05df1b7a576d8d150948e
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 21:24:27 +0530

    9:24

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
+const http = require('http');
 
-/*
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    if (process.env.mediavaletapp != undefined) {
-        res.write(process.env.mediavaletapp);
-        res.wrtie(process.env.mediavaletapp);
-    } else {
-        res.write("Null ");
-        res.wrtie("Null");
-    }
-    
-});
-/*io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
-});*/
-/*http.listen(port, function () {
-    console.log('listening on ' + port);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
 
-*/
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-    if (process.env.mediavaletapp != undefined) {
-        res.send(process.env.mediavaletapp);
-        res.send(process.env.mediavaletapp);
-    } else {
-        res.send('Value is null');
-    }
-});
-app.listen(8081, function () {
-    console.log('Example app listening on port 3000!');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
