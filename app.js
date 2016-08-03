07d6e28885f5355e459c18f469e03f682237a899
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 20:56:09 +0530

    8:56

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    if (process.env.ServiceBusConnectionString != undefined) {
+        res.write(process.env.ServiceBusConnectionString);
+        res.wrtie(process.env.ServiceBusConnectionString);
+    } else {
+        res.write("Null ");
+        res.wrtie("Null");
+    }
+    
 });
-io.on('connection', function (socket) {
+/*io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
-});
+});*/
 http.listen(port, function () {
     console.log('listening on ' + port);
 });
