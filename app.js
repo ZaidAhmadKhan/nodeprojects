3bf5d8f1367d459cfdfbad5a3f3601fcd7e96c9a
zaidahmadkhan <khan_zaid90@outlook.com>
2016-08-03 19:31:06 +0530

    3 Aug 7:31

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 443;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
